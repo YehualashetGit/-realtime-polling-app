f7f03279dfd982ef65988d1229acc469e737e75d
yehualashset abebe <yehualashetab@gmail.com>
2020-07-18 18:16:34 +0530

    deploy to Github

---
+++ src/graphql/queries.js
@@ -0,0 +1,82 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getPoll = /* GraphQL */ `
+  query GetPoll($id: ID!) {
+    getPoll(id: $id) {
+      id
+      name
+      type
+      itemType
+      candidates {
+        items {
+          id
+          pollCandidatesId
+          image
+          name
+          upvotes
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listPolls = /* GraphQL */ `
+  query ListPolls(
+    $filter: ModelPollFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPolls(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        type
+        itemType
+        candidates {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const getCandidate = /* GraphQL */ `
+  query GetCandidate($id: ID!) {
+    getCandidate(id: $id) {
+      id
+      pollCandidatesId
+      image
+      name
+      upvotes
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listCandidates = /* GraphQL */ `
+  query ListCandidates(
+    $filter: ModelCandidateFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listCandidates(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        pollCandidatesId
+        image
+        name
+        upvotes
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
